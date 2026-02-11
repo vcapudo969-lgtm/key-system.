8c48566f65070f95b796f9b108b9501cadc964b7
vcapudo969-lgtm <vcapudo969@gmail.com>
2026-02-11 14:18:37 -0300

    Update Firebase configuration with new API key and domain

--- js/firebase.js
+++ js/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_AQUI",
-  authDomain: "key-system-39e11",
+  apiKey: "AIzaSyB4uXZEikg_YVU85Oo89J0NQRpZkfjkdQI",
+  authDomain: "key-system-39e11.firebaseapp.com",
   projectId: "key-system-39e11",
-  storageBucket: "https://key-system-39e11.firebasestorage.app",
+  storageBucket: "key-system-39e11.appspot.com",
   messagingSenderId: "1027153056618",
   appId: "1:1027153056618:web:0fb78d2e497a220328cb13",
-measurementld: "G-WJFSPYLFJ5"
+  measurementId: "G-WJFSPYLFJ5"
 };
 
 const app = initializeApp(firebaseConfig);
